37b6c8132d7598b92803323ce7c67560b05d89f0
TBtheCoder <boyomichaelbidemi@gmail.com>
2022-11-24 16:23:22 +0100

    static

--- ugofinance/settings.py
+++ ugofinance/settings.py
@@ -91,11 +91,14 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-STATIC_ROOT = BASE_DIR / 'staticfiles' 
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATIC_URL = '/static/'
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
